514c83ed795889bd1fb90f5d36d25970906b82e5
Pedro <pedroh.renner@gmail.com>
2024-03-28 19:05:17 -0300

    first commit

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,13 @@
+<h3>Fornecedor</h3>
+
+{{-- Comentário porra --}}
+<?= 'Texto' ?>
+
+@php
+    /*if(isset($variavel)) {}*/
+@endphp
+
+Fornecedor: {{ $fornecedores[0]['nome']}}
+<br>
+Status: {{ $fornecedores[0]['status']}}
+<br>
